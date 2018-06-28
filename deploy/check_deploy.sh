e6e838342a457d790520742f0d71b60b4ea552a4
fashaun <fashionshow0@gmail.com>
2018-06-28 04:20:44 -0400

    * Add deployment check
     Changes to be committed:
    	modified:   deploy/check_deploy.sh
    	modified:   install.sh

--- deploy/check_deploy.sh
+++ deploy/check_deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo "--Check System Deployment--"
 echo "--Check Network Deployment--"
-echo "Check Control Interface"
+echo "Check management network Interface ..."
 ctrl_if="$(ifconfig | grep tap404)"
-[ -z "$ctrl_if" ] && echo "Please Check management network : openvpn connect failed" && exit
+while true;
+do
+    echo "Wait for management network ready or press [Ctrl-c] to exit ..."
+    [ -z "$ctrl_if" ] && echo "Please Check management network : openvpn connect failed" && continue
+    sleep 1
+done
 echo "Check deploy success"
--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@
 # 6. nvm
 # 7. node
 # 8. newman  
-
 ./deploy/system/debian/gen_log_config.sh
 
 # Define log file
@@ -22,25 +21,25 @@ install_err_log="/tmp/auto_test_system/install_err.log"
 exec 3>&1 1>$install_log
 exec 3>&2 2>$install_err_log
 
-
 echo "Check Current Directory : $(pwd)"
 ./deploy/system/debian/user-data.sh
 
 # Check apt-get install package
 ./deploy/check_apt-get.sh
-
 # Close fd3
 exec 1>&3 2>&3 3>&-
 # Setup mgmt network
-PS3="MRC-AutoTest: "
+PS3="MRC-AutoTest: (select 1-3)"
 echo -e "Select your test server ... \n"
+echo "======README.md====="
 cat config/README.md
+echo "===================="
 select FILENAME in config/ovpn/*;
 do
     case $REPLY in
     "1"|"2"|"3") 
-        exec 3>&1 1>$install_log
-        exec 3>&2 2>$install_err_log
+        exec 3>&1 1>>$install_log
+        exec 3>&2 2>>$install_err_log
         openvpn $FILENAME &
         break
     ;;
@@ -49,6 +48,7 @@ do
     # Debug
     #echo "You picked $FILENAME ($REPLY), it is now only accessible to you."
 done
+
 
 # Check deploy
 ./deploy/check_deploy.sh
