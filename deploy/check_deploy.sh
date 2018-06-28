d792b8818047b0f7d759b4d7bcf92f66a175b045
fashaun <fashionshow0@gmail.com>
2018-06-28 04:29:23 -0400

    * Add deploy check log
     Changes to be committed:
    	modified:   deploy/check_deploy.sh

--- deploy/check_deploy.sh
+++ deploy/check_deploy.sh
@@ -2,11 +2,15 @@
 echo "--Check System Deployment--"
 echo "--Check Network Deployment--"
 echo "Check management network Interface ..."
-ctrl_if="$(ifconfig | grep tap404)"
 while true;
 do
-    echo "Wait for management network ready or press [Ctrl-c] to exit ..."
-    [ -z "$ctrl_if" ] && echo "Please Check management network : openvpn connect failed"
+    ctrl_if="$(ifconfig | grep tap404)"
+    if [ -z "$ctrl_if" ]; then
+        echo "Wait for management network ready or press [Ctrl-c] to exit ..."
+    else
+        echo "Check openvpn success"
+        continue
+    fi
     sleep 1
 done
 echo "Check deploy success"
